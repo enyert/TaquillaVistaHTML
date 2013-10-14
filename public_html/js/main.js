32fc5ee61ae661b6fc0582a7fe8e5f643791ad47
Enyert Vinas <enyertmule@gmail.com>
2013-10-14 16:31:20 -0430

    Vistas HTML - Agregado cambios para responsive

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,43 @@
+/* la clase Taquilla contiene todos los datos importantes de la taquilla de
+ * apuestas lista para el "prototipaje" de la misma
+ * 
+ * @param {type} id
+ * @param {type} centroApuestaId
+ * @param {type} nombre
+ * @param {type} username
+ * @param {type} password
+ * @param {type} created
+ * @param {type} modified
+ * @param {type} status
+ * @param {type} serial
+ * @param {type} anular
+ * @param {type} statusReg
+ * @param {type} admin
+ * @returns {Taquilla}
+ * 
+ **/
 
+function Taquilla(id, centroApuestaId, nombre, username, password, created, modified, status, serial, anular,
+                    statusReg, admin){
+                        this.id = id;
+                        this.centroApuestaId = centroApuestaId;
+                        this.nombre = nombre;
+                        this.username = username;
+                        this.password = password;
+                        this.created  = created;
+                        this.modified = modified;
+                        this.status = status;
+                        this.serial = serial;
+                        this.anular = anular;
+                        this.statusReg = statusReg;
+                        this.admin = admin;
+                    }
+                    
+/**
+ * @argument {int} idBoleto almacena el id del boleto que sera vendido
+ * */
+Object.prototype.venderBoleto = function(idBoleto){
+    if(id.isPrototypeOf(Object)){
+        
+    }
+}                 
